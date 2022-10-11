44df150fc3918da26281a4efe1fbcdf2744599bb
anderson <andersonbrenotec@gmail.com>
2022-10-11 16:57:59 -0300

    Iniciado processo de inserção de dados do banco para o template ...

--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Task
+
+def taskList(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/list.html', {'tasks': tasks})
+
 
 def helloWorld(request):
     return HttpResponse('Aqui pode ser retornado uma reposta http, como também um template html com todas suas informações.')
-
-
-def taskList(request):
-    return render(request, 'tasks/list.html')
 
 
 def yourName(request, name):
